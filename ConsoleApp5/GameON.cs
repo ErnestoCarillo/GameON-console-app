168f640cb6991645480461e88b053efa7375a1f9
ErnestoCarillo <ernestfurtak@onet.pl>
2023-02-22 17:51:31 +0100

    Add project files.

---
+++ ConsoleApp5/GameON.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class GameON
+    {
+        public string GetOutput(int number)
+        {
+            if (number % 15 == 0)
+                return "GameON";
+            if (number % 5 == 0)
+                return "Game";
+            if (number % 3 == 0)
+                return "ON";
+
+            return number.ToString();
+        }
+    }
+}
---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome in GameON");  
+            var GameON = new GameON();
+
+            while (true)
+            {
+                Console.WriteLine("Give the number");
+                var number = GetNumber();
+                Console.WriteLine(GameON.GetOutput(number));             
+                Console.WriteLine();
+            }
+        }
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                
+                    
+                if (!int.TryParse(Console.ReadLine(), out int number))
+                {
+                    Console.WriteLine("You entered wrong date");
+                    continue;
+                }
+
+               
+                    return number;
+            }
+        }
+
+    }
+}
